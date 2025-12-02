dd894c25323f1d217d25269b99e705b2745f4e37
Philip Szalla <philip@szalla.de>
2025-12-03 00:33:22 +0100

    feat: solved day 2

---
+++ src/day2.rs
@@ -0,0 +1,99 @@
+pub fn day2(puzzle: String, part2: bool) -> i64 {
+    let mut sum = 0;
+
+    let mut invalid_fn: fn(i64) -> bool = invalid_id;
+    if part2 {
+        invalid_fn = invalid_id_part2;
+    }
+
+    let ranges = puzzle.split(",");
+    for range in ranges {
+        let parts = range.split("-");
+        let start = parts.clone().nth(0).unwrap().parse::<i64>().unwrap();
+        let end = parts.clone().nth(1).unwrap().parse::<i64>().unwrap();
+
+        for num in start..=end {
+            if invalid_fn(num) {
+                sum += num;
+            }
+        }
+    }
+
+    sum
+}
+
+fn invalid_id(num: i64) -> bool {
+    let len = num_len(&num);
+
+    if len % 2 != 0 {
+        // No pattern possible
+        return false;
+    }
+
+    let pattern_len = len / 2;
+
+    let last = sub_int(&num, pattern_len, 0);
+    let next = sub_int(&num, pattern_len, 1);
+
+    if next == last {
+        return true;
+    }
+
+    false
+}
+
+fn invalid_id_part2(num: i64) -> bool {
+    let len = num_len(&num);
+
+    if len < 2 {
+        // No pattern possible
+        return false;
+    }
+
+    for pattern_len in 1..=(len / 2) {
+        let mut invalid = true;
+
+        if len % pattern_len != 0 {
+            // No equal pattern possible
+            continue;
+        }
+
+        let last = sub_int(&num, pattern_len, 0);
+        for pattern_index in 1..(len / pattern_len) {
+            let next = sub_int(&num, pattern_len, pattern_index);
+
+            if next != last {
+                invalid = false;
+                break;
+            }
+        }
+
+        if invalid {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn num_len(num: &i64) -> i32 {
+    let mut copy = *num;
+    let mut len = 1;
+
+    while copy > 9 {
+        copy /= 10;
+        len += 1;
+    }
+
+    len
+}
+
+fn sub_int(num: &i64, len: i32, index: i32) -> i64 {
+    let moved_num = num / 10i64.pow((index * len) as u32);
+
+    if num_len(&moved_num) == len {
+        return moved_num;
+    }
+
+    moved_num - moved_num / 10i64.pow(len as u32) * 10i64.pow(len as u32)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 mod day1;
+mod day2;
 
 fn main() {
     println!("Hello, world!");
@@ -14,4 +15,12 @@ fn main() {
 
     let res_day1_1 = day1::day1_1(input_day1);
     println!("Day1.1 Result: {}", res_day1_1);
+
+    let input_day2 = fs::read_to_string("day2.txt").unwrap();
+    let res_day2 = day2::day2(input_day2, false);
+    println!("Day2 Result: {}", res_day2);
+
+    let input_day2 = fs::read_to_string("day2.txt").unwrap();
+    let res_day2 = day2::day2(input_day2, true);
+    println!("Day2.1 Result: {}", res_day2);
 }
